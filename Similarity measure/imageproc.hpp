a19c1a329570b163082637bee337924380e52d4f
zl-su <su-zl@seu.edu.cn>
2018-07-13 11:45:09 +0800

    Add interpolation functions

--- Similarity measure/imageproc.hpp
+++ Similarity measure/imageproc.hpp
@@ -18,6 +18,9 @@ this program. If not, see <http://www.gnu.org/licenses/>.
 ****************************************************************************/
 #pragma once
 #include <type_traits>
+#ifndef MATRICE_HOST_FINL
+#define MATRICE_HOST_FINL __forceinline
+#endif
 namespace dgelom {
 
 template<typename _Pixty, typename = typename std::enable_if<std::is_arithmetic<_Pixty>::value>::type>
@@ -41,4 +44,56 @@ void _Grayscale_stretch(_Pixty* Img, std::size_t rows, std::size_t cols)
 		*_Begin = pixel_t(_Scal * (*_Begin - _Min));
 }
 
+template<typename _Valty> MATRICE_HOST_FINL
+_Valty _Tidy_axis(_Valty x, int limx, int& x0, int& x1)
+{
+	const auto ix = static_cast<int>(x);
+	if (ix < 0) { x0 = x1 = 0; return _Valty(0); }
+	if (ix > limx - 2) { x0 = x1 = limx - 1; return _Valty(1); }
+	x0 = ix, x1 = ix + 1; return _Valty(x1 - x);
+}
+//_Imty - image type, _Gdty - matrix type; If they are user defined type, cols() and rows() methods should be implemented for accessing the number of column, and the number of row. In addition, operator()(size_t _Col, size_t _Row) is neccessary for accessing the element at position (_Col, _Row).
+//img - image, gx - gradient in x direction, gy - gradient in y direction, x[2] - coordinate of interpolation point for input, which will be overwritten by the interpolated gradients; return interpolated image intensity.
+template<typename _Imty, typename _Gdty, typename _Valty>
+_Valty _Bilinear_interp(const _Imty& img, const _Gdty& gx, const _Gdty& gy, _Valty x[2])
+{
+	using Image_t = _Imty;
+	using Matrx_t = _Gdty;
+	using pixel_t = typename Image_t::value_t;
+	using value_t = std::common_type_t<typename Matrx_t::value_t, _Valty>;
+
+	int x0, x1, y0, y1;
+	auto dx = _Tidy_axis(x[0], img.cols(), x0, x1);
+	auto dy = _Tidy_axis(x[1], img.rows(), y0, y1);
+	auto dxt = 1.0 - dx, dyt = 1.0 - dy;
+	auto _Interp = [&](auto _F11, auto _F12, auto _F21, auto _F22)->auto 
+	     { return dy * (dx*_F11 + dxt*_F12) + dyt*(dx*_F21 + dxt*_F22); };
+
+	auto Gx11 = value_t(gx(x0, y0)), Gx12 = value_t(gx(x1, y0));
+	auto Gx21 = value_t(gx(x0, y1)), Gx22 = value_t(gx(x1, y1));
+	x[0] = _Interp(Gx11, Gx12, Gx21, Gx22);
+
+	auto Gy11 = value_t(gy(x0, y0)), Gy12 = value_t(gy(x1, y0));
+	auto Gy21 = value_t(gy(x0, y1)), Gy22 = value_t(gy(x1, y1));
+	x[1] = _Interp(Gy11, Gy12, Gy21, Gy22);
+
+	auto I11 = value_t(img(x0, y0)), I12 = value_t(img(x1, y0));
+	auto I21 = value_t(img(x0, y1)), I22 = value_t(img(x1, y1));
+	return _Interp(I11, I12, I21, I22);
+}
+
+// Value of k-th B-Spline basic function at t.
+template<typename _T, typename = std::enable_if_t<std::is_arithmetic_v<_T>>>
+MATRICE_HOST_FINL constexpr _T _Bspline_kernel(size_t k, _T t) {
+	assert(0 <= t && t < 1);
+	assert(k < 4);
+
+	switch (k) {
+	case 0: return (t * (t * (-t + 3) - 3) + 1) / 6;
+	case 1: return (t * t * (3 * t - 6) + 4) / 6;
+	case 2: return (t * (t * (-3 * t + 3) + 3) + 1) / 6;
+	case 3: return t * t * t / 6;
+	default: return 0;
+	}
+}
 }
